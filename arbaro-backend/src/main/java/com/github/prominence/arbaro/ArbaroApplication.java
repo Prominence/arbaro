e32b74873508fdc93b4de10db87aabeb9c4959a4
Alexey Zinchenko <alexey.zinchenko@protonmail.com>
2020-03-08 23:02:34 +0300

    Submodules initialization.

---
+++ arbaro-backend/src/main/java/com/github/prominence/arbaro/ArbaroApplication.java
@@ -0,0 +1,13 @@
+package com.github.prominence.arbaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArbaroApplication.class, args);
+	}
+
+}
---
+++ arbaro-backend/src/test/java/com/github/prominence/arbaro/ArbaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.prominence.arbaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArbaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
